27c5c9850619df8ea731f2630302afc017a5ce03
Arvind Purohith <arvindpurohith@Arvinds-MacBook-Air.local>
2021-03-03 16:16:55 +0530

    Removed everything for a frontend folder issue

--- environments/certificate_tool/cert_apis/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Managers
-
-class ManagerSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Managers 
-        fields = ('pk', 'name', 'email', 'phone', 'no_of_spot_awards')
